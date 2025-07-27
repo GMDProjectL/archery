181688d9e4056e633500a1c6604a7bee2b240fbc
Andrew <kezin636@gmail.com>
2025-07-27 11:49:01 +0300

    clang yapped at me for no obvious reason 🤷‍♂️

--- core/include/archery.hpp
+++ core/include/archery.hpp
@@ -1,4 +1,3 @@
 #pragma once
-#include <iostream>
 
 void say_hello();
--- core/src/core.cpp
+++ core/src/core.cpp
@@ -1,4 +1,5 @@
 #include <archery.hpp>
+#include <iostream>
 
 void say_hello(){
     std::cout << "Hello, from archery core!\n";

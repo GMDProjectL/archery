25309cf2d0b02545f9a157b278dddf5e8b25cc20
Andrew <kezin636@gmail.com>
2025-07-27 16:53:58 +0300

    mib

--- cli/src/main.cpp
+++ cli/src/main.cpp
@@ -5,10 +5,13 @@
 
 int main(int, char**){
     std::string dbName = "core";
-    
+
     auto packages = get_remote_packages(dbName);
     for (auto& package : packages) {
-        std::cout << std::format("{} (version {}) - {} bytes", package.m_name, package.m_version, package.m_uncompressedSize) << std::endl;
+        std::cout << std::format(
+            "{} (version {}) - {:.2f} MiB\n", 
+            package.m_name, package.m_version, (package.m_uncompressedSize / 1024.f / 1024.f)
+        );
     }
     return 0;
 }

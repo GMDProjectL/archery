2811116b40f8551234dae9665466c0c739c5490c
Andrew <kezin636@gmail.com>
2025-07-27 16:46:43 +0300

    core

--- cli/src/main.cpp
+++ cli/src/main.cpp
@@ -5,6 +5,7 @@
 
 int main(int, char**){
     std::string dbName = "core";
+    
     auto packages = get_remote_packages(dbName);
     for (auto& package : packages) {
         std::cout << std::format("{} (version {}) - {} bytes", package.m_name, package.m_version, package.m_uncompressedSize) << std::endl;
